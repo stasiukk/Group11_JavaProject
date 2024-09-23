832950a3d0d59c432765e40ed1a152db1682cc5f
stasiukk <andriistasiuk26@gmail.com>
2024-09-23 10:53:21 -0400

    updated name files and added week 3 practice

--- src/andrii/Week_3_Java_Practice.java
+++ src/andrii/Week_3_Java_Practice.java
@@ -1,8 +1,20 @@
 package andrii;
 
-public class week_3 {
+public class Week_3_Java_Practice {
 
-    //code
+    //create main method
+    public static void main(String[] args) {
+        // Swap two variables' values without using a third variable
+        int a = 10, b = 20;
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
 
-    //new code
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+
+    }
+
+
 }
