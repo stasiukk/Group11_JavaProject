bf5b1b89dcd7c19d7e9f9e8121506691f1e3850c
Kevin Beauvais <kevin2395beauvais@gmail.com>
2024-10-02 14:41:34 -0400

    java practice week03 added

--- src/kevin/week03_javaPractice.java
+++ src/kevin/week03_javaPractice.java
@@ -11,12 +11,12 @@ public class week03_javaPractice {
         int num1 = input.nextInt();
         System.out.println("Enter your second number: ");
         int num2 = input.nextInt();
-        ArrayList<Integer>list = new ArrayList();
+        ArrayList<Integer>list = new ArrayList<>();
         list.add(num1);
         list.add(num2);
 
         System.out.println("Your numbers are: "+list);
-        ArrayList<Integer>list2 = new ArrayList();
+        ArrayList<Integer>list2 = new ArrayList<>();
         list2.add(num2);
         list2.add(num1);
         System.out.println("Your numbers swap: "+list2);
