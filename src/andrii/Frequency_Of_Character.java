a5d5d9ebf5e03c604533e000ec1316fa2562ab9a
stasiukk <andriistasiuk26@gmail.com>
2024-10-14 13:44:48 -0400

    added new java file

--- src/andrii/Frequency_Of_Character.java
+++ src/andrii/Frequency_Of_Character.java
@@ -5,46 +5,38 @@ import java.util.Map;
 
 public class Frequency_Of_Character {
 
+        /*
+        Frequency of Character
+
+        Create a method that will determine the frequency of every character in the given String
+            parameter: String
+            return: void
+
+        Ex:
+            input:
+                frequencyOfCharacter("banana apple pie")
+            output:
+                {b=1, a=4, n=2,  =2, p=3, l=1, e=2, i=1}
+     */
+
     public static void frequencyOfCharacter(String input) {
 
+        //Create a LinkedHashMap to store character counts in order
+        Map<Character, Integer> charsCharacterMap = new LinkedHashMap<Character, Integer>();
 
-        //   step 1 : asking printing  output is in "key and value" format so we can use
-        //   LinkedHashMap<> format that let us print by order(not HashMap)
-        //key-will be character
-        //value-will be count of how many character occurs in the given String
-
-        //   step2 : need to declare given String and HashMap
-
-        String str = input;
-        Map<Character, Integer> characterCount = new LinkedHashMap<>();
-//           key       value
+        //Loop through each character in the string
+        for (char c : input.toCharArray()) {
+            //Update the frequency of the character in the map
+            charsCharacterMap.put(c, charsCharacterMap.getOrDefault(c, 0) + 1);
 
 
-        //step 3 we are going to use for loop to go through each character in the string
-
-        for (int i = 0; i < str.length(); i++) {
-            Character ch = str.charAt(i);
-
-            //help us add character to map
-            if (characterCount.containsKey(ch)) {
-                characterCount.put(ch, characterCount.get(ch) + 1);
-            } else {
-                characterCount.put(ch, 1);
-            }
         }
-
-        // Print the result in "key=value" format
-        System.out.println(characterCount);
+        //Print the frequency map
+        System.out.println(charsCharacterMap);
     }
 
     public static void main(String[] args) {
-
-
+        // Test the method with an example string
         frequencyOfCharacter("banana apple pie");
-
-
     }
-
-
 }
-
